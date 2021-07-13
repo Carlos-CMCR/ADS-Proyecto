618813705fbf6441a9d19b8d864e1cddbd05b181
usertest-thom <thom.maurick.r@gmail.com>
2021-07-13 15:19:32 -0500

    header

--- shared/formulario.php
+++ shared/formulario.php
@@ -33,6 +33,22 @@ class formulario {
             <title><?php echo $titulo?></title>
         </head>
         <body>
+        <header class="main-header">
+            <div class="main-header__block l-container">
+                <div class="main-logo">
+                    <img class="main-logo__img" src="../img/logo.png" alt=""/>
+                    <h1 class="main-logo__title">PUBLYSHER</h1>
+                </div>
+                <nav class="main-menu__nav" id="main-menu__nav" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
+                    <ul class="main-menu__ul">
+                        <li class="main-menu__item" itemprop="url"><a class="main-menu__link" href="#" itemprop="name">Home</a></li>
+                        <li class="main-menu__item" itemprop="url"><a class="main-menu__link" href="#" itemprop="name">Taller De Emprendedores</a></li>
+                        <li class="main-menu__item" itemprop="url"><a class="main-menu__link" href="#" itemprop="name">Workshops</a></li>
+                        <li class="main-menu__item" itemprop="url"><a class="main-menu__link" href="#" itemprop="name">Expositores</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
         <?php 
     }
     protected function piePaginaShow(){
