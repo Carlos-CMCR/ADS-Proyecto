31e2feb70126f44ea960be4b0109a3d161c08167
Marcoar02 <marcoar0298@gmail.com>
2021-07-21 02:21:51 -0500

    thom me la suda

--- model/FacadeViews.php
+++ model/FacadeViews.php
@@ -1,14 +1,9 @@
 <?php 
 include_once("FactoryViews.php");
 class FacadeViews {
-    
     public function getFormAutenticarUsuario(){
         return FactoryViews::getView("AutenticarUsuario");
     }
-
-    
-
-
 }
 
 
