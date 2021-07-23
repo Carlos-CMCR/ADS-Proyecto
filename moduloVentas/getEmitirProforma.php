5b45f4b87ff00bad5d66ed4abd3d521ecf2a124e
usertest-thom <thom.maurick.r@gmail.com>
2021-07-23 02:01:36 -0500

    emitir proforma

---
+++ moduloVentas/getEmitirProforma.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+if(isset($_POST["btnEmitirProforma"])){
+    echo "todo bien";
+}else{
+    include_once("../shared/formMensajeSistema.php");
+    $nuevoMensaje = new formMensajeSistema;
+    $nuevoMensaje -> formMensajeSistemaShow("¡ACCESO NO PERMITIDO!","<a href='../index.php' class='form-message__link'>Volver</a>");
+}
+
+?>
