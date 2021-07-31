49ce6f628bfa8c1473eb8633e36fc96d523cea27
usertest-thom <thom.maurick.r@gmail.com>
2021-07-31 18:17:17 -0500

    plantilla comprobante

--- shared/comprobante_plantilla.php
+++ shared/comprobante_plantilla.php
@@ -7,6 +7,14 @@ class comprobante_plantilla {
     public function obtenerHTML($proforma = []){
     }
     public function generarPDF($html,$codigo = ''){
+        $pdfOptions = new Options();
+        $pdfOptions->set('isRemoteEnabled', true);
+        //$pdfOptions->set('isHtml5ParserEnabled', true);
+        $dompdf = new Dompdf($pdfOptions);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream("comprobante_".$codigo.".pdf",array('Attachment'=>0));
     }
 }
 
