e01f58ea8ad175291d644e1778bba1da555e19f0
usertest-thom <thom.maurick.r@gmail.com>
2021-07-31 18:10:41 -0500

    comprobante plantilla

---
+++ shared/comprobante_plantilla.php
@@ -0,0 +1,13 @@
+<?php 
+require_once 'dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class comprobante_plantilla {
+    public function obtenerHTML($proforma = []){
+    }
+    public function generarPDF($html,$codigo = ''){
+    }
+}
+
+?>
